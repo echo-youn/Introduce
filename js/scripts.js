2160560dca01191f7e5ee15eb6e3c6b2b4be3de3
윤승재 <tmdwo0727@naver.com>
2020-06-11 10:41:10 +0900

    Update IE Check

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+const browser = isBrowserCheck();
 /*!
     * Start Bootstrap - Resume v6.0.0 (https://startbootstrap.com/template-overviews/resume)
     * Copyright 2013-2020 Start Bootstrap
@@ -40,3 +41,11 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+function isIE(){ 
+    if ((navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1)) { 
+        // IE 일 경우
+        alert('Internet Explorer에서 접속하였습니다. 원활한 이용을 원하실 경우 다른 브라우저를 이용해주세요.');
+        return;
+    }
+}
